ef1d99ecf7856e63674c96ab17d07e0594a47e69
Cody Anderson <cody.anderson@scorevision.com>
2025-12-08 16:13:25 -0600

    add bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { motion } from "motion/react"
 import leafLeft from './assets/leaf_left.png'
+import tiledBg from './assets/autumn_9.jpg'
 import './App.css'
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
   }
 
   return (
-    <div className="door-container" key={key}>
+    <div className="door-container" key={key} style={{ backgroundImage: `url(${tiledBg})` }}>
       {/* Left door */}
       <motion.div
         className="door door-left"
@@ -32,26 +33,27 @@ function App() {
         <img src={leafLeft} alt="" className="door-image door-image-flipped" />
       </motion.div>
 
-      {/* Centered header */}
-      <motion.h1
-        className="header"
-        initial={{ scale: 1.3, opacity: 0 }}
-        animate={{ scale: 1, opacity: 1 }}
-        transition={{ duration: 1.5, ease: "easeOut", delay: 0.5 }}
-      >
-        Welcome
-      </motion.h1>
+      {/* Content area */}
+      <div className="content-column">
+        <motion.h1
+          className="header"
+          initial={{ scale: 1.3, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 1.5, ease: "easeOut", delay: 0.5 }}
+        >
+          Lauren & Cody
+        </motion.h1>
 
-      {/* Reset button */}
-      <motion.button
-        className="reset-button"
-        onClick={resetAnimation}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 2 }}
-      >
-        Reset
-      </motion.button>
+        <motion.button
+          className="reset-button"
+          onClick={resetAnimation}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 2 }}
+        >
+          Reset
+        </motion.button>
+      </div>
     </div>
   )
 }
